84e503d6423b6fec10ab3dee26ccbbbcd25cf7f2
kattykitty21 <kattykitty21@gmail.com>
2022-08-10 21:51:22 +0300

    refactoring

---
+++ src/test/java/errorTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class errorTest {
+    @Test
+    void firstTest() {
+        Assertions.assertFalse(3<1);
+    }
+}
