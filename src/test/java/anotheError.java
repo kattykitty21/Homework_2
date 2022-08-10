9b208d9c81cf65c00dbc7fe716039ec5301b0866
kattykitty21 <kattykitty21@gmail.com>
2022-08-10 22:00:55 +0300

    change operator

---
+++ src/test/java/anotheError.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class anotheError {
+
+    @Test
+    void anotheError() {
+        Assertions.assertTrue(3>=1);
+    }
+}
